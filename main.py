f468877b85416e1db3f421e78c505b856c4ac14b
Lepidopter <31863749+sergis4ake@users.noreply.github.com>
2023-06-07 18:44:20 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi import Header
+from pydantic import BaseModel
+
+app = FastAPI()
+port = 8080
+
+class FibonacciRequest(BaseModel):
+    index: int
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get('/health')
+def health_check():
+    headers = {"System-Health": "true"}
+    return Response(status_code=204, headers=headers)
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+@app.post('/fibonacci')
+def calculate_fibonacci(request: FibonacciRequest):
+    fib_index = request.index
+    result = fibonacci(fib_index)
+    return {"index": fib_index, "result": result}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port)
